91ec73d083362088fa40775f75c3429d517d5182
fdammeke <fabian.dammekens@cegeka.be>
2012-11-26 08:34:39 +0100

    Initial cleaned appserver install release

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -4,4 +4,31 @@ class sunappserver::redhat {
     ensure   => $sunappserver::real_appserver_ensure,
   }
 
+  file { '/etc/sysconfig/sunappserver' :
+    ensure    => file,
+    content   => template('sunappserver/sunappserver-sysconfig.erb'),
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0644',
+  }
+
+  service { 'sunappserver' :
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    require   => Service['imq'],
+  }
+
+  service { 'imq' :
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    require   => Package['sunappserver'],
+  }
+  exec { 'modify-ownership-for-appserver-root' :
+    command => "/bin/chown -R ${sunappserver::runas} ${sunappserver::params::appserv_installroot}",
+    onlyif  => "/usr/bin/test `/usr/bin/stat -c %U ${sunappserver::params::appserv_installroot} | grep ${sunappserver::runas}`",
+    require => Package['sunappserver'],
+  }
+
 }
--- spec/classes/sunappserver_spec.rb
+++ spec/classes/sunappserver_spec.rb
@@ -9,30 +9,7 @@ describe 'sunappserver' do
 
     context "and with appserver_version => 9.1.01" do
       let (:params) {{ :appserver_version => '9.1.01' }}
-      
-      it { should contain_package('sunappserver').with_ensure('9.1.01') }
-    end
-
-    context "and without appserver_version parameter, without ensure parameter" do
-      it { should contain_package('sunappserver').with_ensure('latest') }
-    end
-
-    context "and with ensure => present, without appserver_version parameter" do
-      let (:params) {{ :ensure => 'present' }}
-
-      it { should contain_package('sunappserver').with_ensure('latest') }
-    end
-
-    context "and with ensure => absent, without appserver_version parameter" do
-      let (:params) {{ :ensure => 'absent' }}
-
-      it { should contain_package('sunappserver').with_ensure('absent') }
-    end
-
-    context "and with ensure => absent, with appserver_version => 9.1.01" do
-      let (:params) {{ :appserver_version => '9.1.01', :ensure => 'absent' }}
-
-      it { should contain_package('sunappserver').with_ensure('absent') }
+      it { should include_class('sunappserver::redhat') }
     end
 
   end
