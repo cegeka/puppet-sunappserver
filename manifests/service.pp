fb1bec757976f2f48d24c3abe621bedf32ad5ac1
Kristof Willaert <kristof.willaert@cegeka.be>
2014-01-24 16:05:27 +0100

    Only manage service imq when type is remote

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,4 +1,8 @@
-class sunappserver::service($service_state=undef, $imq_state=undef) {
+class sunappserver::service (
+    $service_state = undef,
+    $imq_type = 'remote',
+    $imq_state = undef
+  ) {
 
   include sunappserver::params
 
@@ -9,20 +13,22 @@ class sunappserver::service($service_state=undef, $imq_state=undef) {
     hasstatus => true
   }
 
-  service { 'imq' :
-    ensure    => $imq_state,
-    enable    => true,
-    hasstatus => true
-  }
-
-  case $service_state {
-    'running': {
-      Service['imq'] -> Service['sunappserver']
+  if $imq_type == 'remote' {
+    service { 'imq' :
+      ensure    => $imq_state,
+      enable    => true,
+      hasstatus => true
     }
-    'stopped': {
-      Service['sunappserver'] -> Service['imq']
-    }
-    default: { fail('Class[sunappserver::service]: parameter service_state must be running or stopped') }
-  }
 
+    case $service_state {
+      'running': {
+        Service['imq'] -> Service['sunappserver']
+      }
+      'stopped': {
+        Service['sunappserver'] -> Service['imq']
+      }
+      default: { fail('Class[sunappserver::service]: parameter service_state must be running or stopped')
+      }
+    }
+  }
 }
