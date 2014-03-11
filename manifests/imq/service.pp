2b32775cfc7abd401eb96f5b4ad2e053f96674df
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-03-11 13:09:12 +0100

    Added feature to not manage service for appserver and imq

--- manifests/imq/service.pp
+++ manifests/imq/service.pp
@@ -5,12 +5,14 @@ class sunappserver::imq::service(
 
   case $ensure {
     'running', 'stopped': {
-
       service { 'imq':
         ensure    => $ensure,
         enable    => $enable,
         hasstatus => true
       }
+    }
+    'unmanaged': {
+      notice('Class[sunappserver::imq::service]: service is currently not being managed')
     }
     default: {
       fail('Class[sunappserver::imq::service]: parameter ensure must be running or stopped')
